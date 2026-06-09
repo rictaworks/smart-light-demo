ce57904ce364c26af8253a46c41245c27672bf74
rictaworks <takizawa@rictaworks.jp>
2026-06-10 01:03:33 +0900

    fix: set page title to スマートライトデモ

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Smart Light Demo</title>
+        <title>スマートライトデモ</title>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
